8d01db89a5cc21bdd02b6c2400ae0f47ee708b8f
saranyaraja <saranya2004@gmail.com>
2018-02-19 19:58:44 -0500

    Updates

--- StudentEnrollmentWithSpring/src/main/java/com/github/elizabetht/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.github.elizabetht.service;
-
-import com.github.elizabetht.model.Student;
-
-public interface StudentService {
-	Student save(Student student);
-	boolean findByLogin(String userName, String password);
-	boolean findByUserName(String userName);
-}
